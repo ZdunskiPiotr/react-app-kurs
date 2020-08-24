4f64f0ce8252b9c690492687c6adba660d340b3b
Piotr Zduński <zdunski.piotrek@gmail.com>
2020-08-24 14:08:08 +0200

    Updates

---
+++ precache-manifest.d711f1c104d022da0752eaa96a2f9030.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78bc1946a35bdf6c5e9e98f17ec07f4d",
+    "url": "/react-app-kurs/index.html"
+  },
+  {
+    "revision": "ab668c1c0e2e5defad3f",
+    "url": "/react-app-kurs/static/css/main.e64a5803.chunk.css"
+  },
+  {
+    "revision": "781fa9020d00ebe35efd",
+    "url": "/react-app-kurs/static/js/2.0be00902.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app-kurs/static/js/2.0be00902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab668c1c0e2e5defad3f",
+    "url": "/react-app-kurs/static/js/main.d80707fc.chunk.js"
+  },
+  {
+    "revision": "b98f7a1ef5b40f86e9d8",
+    "url": "/react-app-kurs/static/js/runtime-main.509cd6c7.js"
+  },
+  {
+    "revision": "73fc7eb9d9ce4e10d2799319e300a081",
+    "url": "/react-app-kurs/static/media/background.73fc7eb9.jpg"
+  },
+  {
+    "revision": "1e6c41b21450561892d1cf8420f36a8b",
+    "url": "/react-app-kurs/static/media/background2.1e6c41b2.jpg"
+  },
+  {
+    "revision": "ddeb51f43c9ac211a9b3d1fcf981cf1c",
+    "url": "/react-app-kurs/static/media/background3.ddeb51f4.jpg"
+  }
+]);
